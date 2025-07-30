c10c4c3aeb2091d95d8d1414bc38c1d550879da2
dkac14 <150403101+dkac14@users.noreply.github.com>
2025-07-30 14:16:45 -0500

    Creación de proyecto

---
+++ calculatorsmp/src/test/java/calculatorsmp/OperationsTest.java
@@ -0,0 +1,12 @@
+package calculatorsmp;
+
+import calculatorsmp.Operations;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class OperationsTest {
+
+    
+    
+}
